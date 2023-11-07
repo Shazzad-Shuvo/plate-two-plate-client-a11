96f06300db3cc501c5890387395d37bee524c793
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-11-07 23:43:00 +0600

    useAxiosSecure custom hook created and axios interceptor implemented

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <h2 className="mt-20 mb-6 text-center text-5xl font-bold">A small contribution can bring a smile</h2>
             <Slider></Slider>
-            {/* <FoodCard></FoodCard> */}
+            <FoodCard></FoodCard>
         </div>
     );
 };
